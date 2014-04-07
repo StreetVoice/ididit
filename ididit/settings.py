69fe3ee9a860359bdeeb1d175b82917207149521
tzangms <tzangms@gmail.com>
2014-04-07 23:26:57 +0800

    update ALLOWED_HOSTS

--- ididit/settings.py
+++ ididit/settings.py
@@ -16,7 +16,7 @@ DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
